295a3472792f18bcab5c7f524182906dd8f9bd6a
Jon Stjärnström <jon.stjarnstrom@frojd.se>
2016-11-04 16:20:55 +0100

    First commit

---
+++ src/js/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Synth from './synth';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Synth />
+            </div>
+        );
+    }
+}
+
+export default App;
